d22e679977d95586e3c090ea7c903a650e321296
En-Ping Hsieh <hsiehenping.fareastone@gmail.com>
2016-12-12 12:57:09 +0800

    Setup UI Layout

--- app/src/main/java/com/codepath/codepath_network_sample/MainActivity.java
+++ app/src/main/java/com/codepath/codepath_network_sample/MainActivity.java
@@ -2,12 +2,22 @@ package com.codepath.codepath_network_sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivSample;
+    private Button btnAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivSample = (ImageView) findViewById(R.id.ivSample);
+        btnAction = (Button) findViewById(R.id.btnAction);
+
+
     }
 }
