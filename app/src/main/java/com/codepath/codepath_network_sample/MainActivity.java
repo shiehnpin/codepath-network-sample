e1f0d64b69ac30025244e8ebdc322d19f3c72ae1
En-Ping Hsieh <hsiehenping.fareastone@gmail.com>
2016-12-12 13:56:22 +0800

    Implement function which is able to get JSON Object from API.

--- app/src/main/java/com/codepath/codepath_network_sample/MainActivity.java
+++ app/src/main/java/com/codepath/codepath_network_sample/MainActivity.java
@@ -5,20 +5,29 @@ import android.graphics.BitmapFactory;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView txIp;
     private ImageView ivSample;
-    private Button btnAction;
+    private Button btnGetImage;
+    private Button btnGetJson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +40,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         );
 
         ivSample = (ImageView) findViewById(R.id.ivSample);
-        btnAction = (Button) findViewById(R.id.btnAction);
-        btnAction.setOnClickListener(this);
+        txIp = (TextView) findViewById(R.id.txIP);
+        btnGetImage = (Button) findViewById(R.id.btnGetImage);
+        btnGetJson = (Button) findViewById(R.id.btnGetIp);
+        btnGetImage.setOnClickListener(this);
+        btnGetJson.setOnClickListener(this);
 
 
     }
@@ -41,10 +53,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        getImageFromAPI();
+        if(v.getId() == R.id.btnGetImage) {
+            getImageFromAPI();
+        }else if(v.getId() == R.id.btnGetIp) {
+            getJsonFromAPI();
+        }
 
     }
 
+    /*
+    Data url: http://httpbin.org/get
+    JSON sample:
+    {
+        "args": {...},
+        "headers": {...},
+        "origin": "123.51.183.156",
+        "url": "http://httpbin.org/get"
+    }
+    */
+
+    private void getJsonFromAPI() {
+        //The links that we can fetch json data.
+        URL targetUrl = null;
+        HttpURLConnection conn = null;
+        InputStream in = null;
+        try {
+            targetUrl = new URL("http://httpbin.org/get");
+
+            //1. Step 1, open an connection linked to the specific url.
+            conn = (HttpURLConnection) targetUrl.openConnection();
+
+            //2. Step 2, open an input stream so that we can fetch data from target url.
+            in = conn.getInputStream();
+
+            //3. Step 3, Read, concat and parse data into JsonObject through stream.
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String tmp ;
+            while ((tmp = reader.readLine())!=null){
+                stringBuilder.append(tmp);
+            }
+            JSONObject object = (JSONObject) new JSONTokener(stringBuilder.toString()).nextValue();
+            txIp.setText(object.getString("origin"));
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        } finally {
+            //Release stream
+            if(in!=null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Release the httpConnection
+            if(conn!=null){
+                conn.disconnect();
+            }
+
+        }
+
+    }
+
+    //Image url: http://httpbin.org/image
     private void getImageFromAPI()  {
 
         //The links that we can fetch image.
